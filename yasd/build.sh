fa3d016244eaae3b77bc14dc097b8d361161eaae
李铭昕 <715557344@qq.com>
2021-03-16 15:06:26 +0800

    release yasd only

--- yasd/build.sh
+++ yasd/build.sh
@@ -4,4 +4,4 @@ docker-compose build yasd
 docker run --name yasd build/yasd
 docker cp yasd:/opt/www/yasd/modules/yasd.so yasd-${PHP_VERSION}-alpine-v${ALPINE_VERSION}.so
 docker rm yasd
-ll
+ls -al
