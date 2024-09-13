14f67effe4df5c386aa85e4348d6fffa28859e65
李铭昕 <715557344@qq.com>
2024-09-13 21:25:10 +0800

    Update

--- wxwork-finance-sdk/build.sh
+++ wxwork-finance-sdk/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-docker-compose build wxwork-finance-sdk
+docker compose build wxwork-finance-sdk
 docker run --name wxwork-finance-sdk build/wxwork-finance-sdk
 docker cp wxwork-finance-sdk:/opt/www/wxwork-finance-sdk.zip wxwork-finance-sdk-${PHP_VERSION}-alpine-v${ALPINE_VERSION}.zip
 docker rm wxwork-finance-sdk
